ecd1f037f2b9f8be94bab59f69012d22abbfc5b7
Alexandros Karalis <karalisalexandros@gmail.com>
2025-04-12 20:38:37 +0300

    feat(CustomPlaceholder): add CustomPlaceholder component to use as loader

---
+++ src/components/CustomPlaceholder/index.tsx
@@ -0,0 +1,6 @@
+import { JSX } from "react";
+import styles from "./index.module.scss";
+
+export const CustomPlaceholder = (): JSX.Element => {
+  return <div className={styles.CustomPlaceholder} />;
+};
