bfa3d21eadd9ee64dbef2d59a3e92b443ee14b5f
Alexandros Karalis <karalisalexandros@gmail.com>
2025-04-12 19:39:18 +0300

    feat(model): add MoviesResponse model

---
+++ src/models/MoviesResponse.ts
@@ -0,0 +1,14 @@
+import { Movie } from "./Movie";
+
+interface Dates {
+  maximum: string;
+  minimum: string;
+}
+
+export interface MoviesResponse {
+  dates: Dates;
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
