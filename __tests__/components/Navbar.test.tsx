55b40376dda91935bb698b988393871bd63433ef
Alexandros Karalis <karalisalexandros@gmail.com>
2025-04-12 21:00:31 +0300

    test(Navbar): set title text from the appConfig file

--- __tests__/components/Navbar.test.tsx
+++ __tests__/components/Navbar.test.tsx
@@ -2,12 +2,13 @@ import "@testing-library/jest-dom";
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import Navbar from "../../src/components/Navbar/Navbar";
+import { appConfig } from "../../src/constants/config";
 
 describe("Navbar", () => {
   it("should contain app title of type h1", () => {
     render(<Navbar />);
 
-    const h1 = screen.getByRole("heading", { name: "CineMagic" });
+    const h1 = screen.getByRole("heading", { name: appConfig.appName });
 
     expect(h1).toBeInTheDocument();
 
