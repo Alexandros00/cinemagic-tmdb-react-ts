3dea18d1784213dfe4c3cb1d8c486bd74366a2cd
Alexandros Karalis <karalisalexandros@gmail.com>
2025-04-12 15:02:11 +0300

    chore(setup): initial project structure

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+/** @type {import('prettier').Options} */
+const config = {
+  semi: true,
+  singleQuote: false,
+  trailingComma: "none",
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 80
+};
+
+export default config;
