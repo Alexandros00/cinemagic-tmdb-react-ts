97e0611bb900f937acfdf1fc77d01f565a2d837a
Alexandros Karalis <karalisalexandros@gmail.com>
2025-04-13 10:29:35 +0300

    feat(error): add ErrorBoundary and Suspense in App.tsx and lazy load NotFound comp

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,25 @@
 import Navbar from "@components/Navbar/Navbar";
 import LandingPage from "./pages/landing-page/LandingPage";
 import { Route, Routes } from "react-router-dom";
-import { lazy } from "react";
+import { lazy, Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
+import { ErrorFallback } from "./components/ErrorFallback";
+import PageLoader from "./components/PageLoader";
 
 const NotFound = lazy(() => import("@components/NotFound"));
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <LandingPage />
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </>
+    <ErrorBoundary FallbackComponent={ErrorFallback}>
+      <Suspense fallback={<PageLoader />}>
+        <Navbar />
+        <LandingPage />
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Suspense>
+    </ErrorBoundary>
   );
 }
 
