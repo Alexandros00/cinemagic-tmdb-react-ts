ca27402275970b0f03ab13a9ce53026b38146ccf
Alexandros Karalis <karalisalexandros@gmail.com>
2025-05-02 08:02:58 +0300

    feat(test): add mock IntersectionObserver

--- __tests__/setupTests.ts
+++ __tests__/setupTests.ts
@@ -1,1 +1,15 @@
 import "@testing-library/jest-dom/vitest";
+
+// Stub IntersectionObserver
+class MockIntersectionObserver {
+  constructor() {}
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+Object.defineProperty(global, "IntersectionObserver", {
+  writable: true,
+  configurable: true,
+  value: MockIntersectionObserver
+});
