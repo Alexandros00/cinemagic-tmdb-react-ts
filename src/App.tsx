681cfb5ee1a437ad8a0586c939cda47af4e2dd3c
Alexandros Karalis <karalisalexandros@gmail.com>
2025-05-20 18:46:06 +0300

    feat(routing): add  /movies/:id route

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import Navbar from "@components/Navbar/Navbar";
 import LandingPage from "@pages/landing-page/LandingPage";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
 import { ErrorFallback } from "@components/ErrorFallback/ErrorFallback";
 import PageLoader from "@/components/PageLoader/PageLoader";
+import GoToTopButton from "./components/GoToTopButton/GoToTopButton";
 
 const NotFound = lazy(() => import("@/components/NotFound/NotFound"));
 
@@ -14,9 +15,11 @@ function App() {
       <Suspense fallback={<PageLoader />}>
         <Navbar />
         <Routes>
-          <Route path="/" element={<LandingPage />} />
+          <Route path="/" element={<Navigate to="/movies" replace />} />
+          <Route path="/movies/:id?" element={<LandingPage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
+        <GoToTopButton />
       </Suspense>
     </ErrorBoundary>
   );
