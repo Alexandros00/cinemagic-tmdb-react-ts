4c8ef83350f2b5fb5af1c59e5b367f1a7c194c4d
Alexandros Karalis <karalisalexandros@gmail.com>
2025-05-20 18:49:01 +0300

    feat(hook): improve hook for infinite scroll movies

--- src/hooks/useGetTrendingMovies.ts
+++ src/hooks/useGetTrendingMovies.ts
@@ -1,11 +1,22 @@
 import { useCallback, useEffect, useMemo, useState } from "react";
 import { Movie } from "../models/Movie";
 import { AxiosError } from "axios";
-import { MoviesResponse } from "../models/MoviesResponse";
+import {
+  MoviesResponse,
+  MoviesResponseMetadata
+} from "../models/MoviesResponse";
 import { getTMDBService } from "@/services/TMDBService";
+import { Params } from "@/models/Params";
 
+const DEFAULT_PARAMS: Params = {
+  page: 1,
+  language: "en-US"
+};
 export const useGetTrendingMovies = () => {
   const [movies, setMovies] = useState<Movie[]>([]);
+  const [params, setParams] = useState<Params>(DEFAULT_PARAMS);
+  const [moviesMetadata, setMoviesMetadata] =
+    useState<MoviesResponseMetadata>();
   const [isLoading, setIsLoading] = useState<boolean>(true);
   const [error, setError] = useState<string | null>(null);
 
@@ -17,15 +28,27 @@ export const useGetTrendingMovies = () => {
   const getMovies = useCallback(
     async (signal: AbortSignal) => {
       try {
-        const response = await service.fetchEntities({ signal });
-        setMovies(response?.results || []);
+        const response = await service.fetchEntities({ signal, params });
+        if (response) {
+          const {
+            results = [],
+            dates = { maximum: "", minimum: "" },
+            page = 0,
+            total_pages = 0,
+            total_results = 0
+          } = response;
+          setMovies((p) => [...p, ...results]);
+          setMoviesMetadata({ dates, page, total_pages, total_results });
+        } else {
+          setMovies([]);
+        }
       } catch (error) {
         console.error("Error fetching movies: ", error);
         setError((error as AxiosError).message);
       }
       setIsLoading(false);
     },
-    [service]
+    [service, params]
   );
 
   useEffect(() => {
@@ -38,6 +61,9 @@ export const useGetTrendingMovies = () => {
 
   return {
     movies,
+    moviesMetadata,
+    params,
+    setPage: (page: number) => setParams((p) => ({ ...p, page })),
     isLoading,
     error
   };
